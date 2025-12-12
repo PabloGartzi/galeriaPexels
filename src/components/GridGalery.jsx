70b83fada78522c41b5b09118698c88989bce6c9
PabloGartzi <pablogartzi02@gmail.com>
2025-12-12 20:48:31 +0100

    Hook de useFetch

--- src/components/GridGalery.jsx
+++ src/components/GridGalery.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import {useApiCall} from "../hooks/useApiCall"
 import {Card} from './Card'
 import {Paginacion} from './Paginacion'
+import { useFetch } from "../hooks/useFetch";
 
 
 export const GridGalery = ({categoria}) => {
@@ -10,6 +11,15 @@ export const GridGalery = ({categoria}) => {
     const [pagina, setPagina] = useState(1); // página actual
     
     const { fotos, totalResultados, cargando } = useApiCall(cate, perPage, pagina);
+
+//ESTO ES CON EL useFetch
+/*     const API_KEY = "l7j5PBtSkZYjGj9uxxw8oqVZE4XFAEt2EV7kp172g5004Nj2t0RtK4aB";
+    const url = `https://api.pexels.com/v1/search?query=${categoria}&per_page=${perPage}&page=${pagina}`;
+    const { data, cargando } = useFetch(url, { 
+        headers: { Authorization: API_KEY } 
+    });
+    const fotos = data?.photos || [];
+    const totalResultados = data?.total_results || 0; */
   return (
     <>
     <div className="galeria">
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
-export const useFetch = () => {
-  return {
-    fotos,
-    cargando
-  }
-}
+export const useFetch = (url, options = {}) => {
+  const [data, setData] = useState(null);
+  const [cargando, setCargando] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (!url) return;
+    setCargando(true);
+    setError(null);
+
+    fetch(url, options)
+      .then((res) => {
+        if (!res.ok) throw new Error("Error en la petición");
+        return res.json();
+      })
+      .then((json) => setData(json))
+      .catch((err) => {
+        console.log("Error");
+        setError(err)
+      })
+      .finally(() => setCargando(false))
+  }, [url]);
+  return { data, cargando };
+};
